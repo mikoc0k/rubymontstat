ac050b72824f0c184a1171d3ff390c17c6cd8c13
uciharis <uciharis@hotmail.com>
2026-03-17 14:26:44 +0700

    bahasan kedua: obj, class, method

---
+++ 002.rb
@@ -0,0 +1,9 @@
+# Object, Class and Method
+
+# di ruby, hampir semua adalah objek
+# contoh yang bukan objek : if, self
+
+# mengecek class tertentu
+
+teksto = "jokoui presiden terbaik di IKN"
+puts teksto.class #string
