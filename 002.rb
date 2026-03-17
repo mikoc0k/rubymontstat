5fbe887623a01d574e8de43526e560667a15074d
uciharis <uciharis@hotmail.com>
2026-03-17 14:36:00 +0700

    penjelasan kecil ttg instance, object, method

--- 002.rb
+++ 002.rb
@@ -7,3 +7,11 @@
 
 teksto = "jokoui presiden terbaik di IKN"
 puts teksto.class #string
+
+puts teksto.is_a?(String) #true
+puts 2222.is_a?(Numeric) #true
+# kalo ketemu istilah instances, anggap = manifest/ perwujudan
+# objek adalah perwujudan nyata dari Class
+# method adalah sifat dari objek
+# terkadang ada method yang melekat pada Class tertentu
+# Ada juga method yang tersedia di smua Class(objek)
